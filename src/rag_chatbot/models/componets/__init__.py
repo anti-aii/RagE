391c151fab5b747095fb6429e52ce7c244aca577
Nguyendat-bit <nduc0231@gmail.com>
2024-02-27 11:15:45 +0700

    add

--- src/rag_chatbot/models/componets/__init__.py
+++ src/rag_chatbot/models/componets/__init__.py
@@ -1,3 +1,4 @@
 from .extra_roberta import ExtraRoberta
 from .attention_context import element_wise, AttentionWithContext 
 from .load_backbone import load_backbone
+from .pool_strategy import PoolingStrategy
