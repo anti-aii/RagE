beea5b9c8efebb132308dbb93bd08a1a985ef181
Nguyendat-bit <nduc0231@gmail.com>
2024-06-30 10:06:47 +0700

    hotfix self.model.tokenizer in trainer.py

--- src/rage/trainer/trainer.py
+++ src/rage/trainer/trainer.py
@@ -274,7 +274,7 @@ class _TrainerLLM(_Trainer):
         super().__init__(model, argument_train= argument_train, argument_dataset= argument_dataset)
         
     def _setup_addtion_config(self):
-        self.collate= GenAnsCollate(self.model.tokenizer, self.max_length, self.advance_config_encode)
+        self.collate= GenAnsCollate(self.model_lm.tokenizer, self.max_length, self.advance_config_encode)
 
     def _setup_dataset(self): 
         train_dataset= GenAnsDL(self.data_train)
@@ -313,7 +313,7 @@ class _TrainerBiEncoder(_Trainer):  ## support
     def _setup_addtion_config(self):
         self.loss= self._select_loss_functon(self.loss_function)
         
-        self.collate= SentABCollate(self.model.tokenizer, mode= "bi_encoder", advance_config_encode= self.advance_config_encode,
+        self.collate= SentABCollate(self.model_lm.tokenizer, mode= "bi_encoder", advance_config_encode= self.advance_config_encode,
                     max_length= self.max_length, task= self.loss.task_name, augment_func= self.augment_data_function)
     
     def _setup_dataset(self):
@@ -387,7 +387,7 @@ class _TrainerCrossEncoder(_Trainer):
     def _setup_addtion_config(self):
         self.loss= self._select_loss_functon(self.loss_function)
         
-        self.collate= SentABCollate(self.model.tokenizer, mode= "cross_encoder", advance_config_encode= self.advance_config_encode,
+        self.collate= SentABCollate(self.model_lm.tokenizer, mode= "cross_encoder", advance_config_encode= self.advance_config_encode,
                     max_length= self.max_length, task= self.loss.task_name, augment_func= self.augment_data_function)
         
     def _setup_dataset(self):
