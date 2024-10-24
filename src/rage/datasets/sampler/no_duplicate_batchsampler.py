ea5f077f100d754af64e55d2c1d911d6ad753338
Nguyendat-bit <nduc0231@gmail.com>
2024-10-24 20:16:18 +0700

    hot fix

--- src/rage/datasets/sampler/no_duplicate_batchsampler.py
+++ src/rage/datasets/sampler/no_duplicate_batchsampler.py
@@ -44,9 +44,9 @@ class NoDuplicatesBatchSampler(Sampler):
         while len(index_dataset) > 0: 
             idx, index_dataset= index_dataset[0], index_dataset[1:]
             if self.obverse=='query': 
-                example= self.dataset.__getitem__(idx.item())[0] # obverse on anchor or query
+                example= list(self.dataset.__getitem__(idx.item()))[0] # obverse on anchor or query
             elif self.obverse=='query_positive': 
-                example= '<type>'.join(self.dataset.__getitem__(idx.item())[:1])
+                example= '<type>'.join(list(self.dataset.__getitem__(idx.item()))[:1])
             
             if example.strip().lower() in text_in_batch:
                 duplicate_queue.append(idx.item())
